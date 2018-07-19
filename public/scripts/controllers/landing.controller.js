1e7e598d06f7376658702ca4da43ef3aff77c66b
Chris Stanton <cstanton@epgmediallc.com>
2018-07-19 15:59:11 -0500

    ng-repeating dummy data

--- public/scripts/controllers/landing.controller.js
+++ public/scripts/controllers/landing.controller.js
@@ -18,6 +18,43 @@ myApp.controller('LandingController',['EmailFactory', 'alertify', '$scope', '$lo
   self.organization = 'EPG Media';
   self.brand = 'EPG Media Central Database';
 
+  self.API_response = [
+    {
+      brand: 'BI',
+      type: 'Enews',
+      campaign: 'Top10',
+      date: '06/04/18',
+      headline: 'May Top 10: Yamaha Marine officially unveils new 425 horsepower V8 outboard, Mercury Marine introduces new V-8, V-6 engine models',
+      deployed: '13187',
+      recieved: '13146',
+      bounce: '0.31%',
+      opens: '4593',
+      unique_opens: '2805',
+      open_rate: '21.34%',
+      clicks: '1224',
+      unique_clicks: '685',
+      click_to_open: '24.42%',
+      ctr: '5.21%'
+    },
+    {
+      brand: 'BW',
+      type: 'Enews',
+      campaign: 'Enews',
+      date: '06/04/18',
+      headline: 'The 2018 Retailers of the Year Part 1',
+      deployed: '8316',
+      recieved: '7735',
+      bounce: '6.99%',
+      opens: '1806',
+      unique_opens: '1263',
+      open_rate: '16.33%',
+      clicks: '1119',
+      unique_clicks: '452',
+      click_to_open: '10.42%',
+      ctr: '4.18%'
+    }
+  ];
+
   self.sendQuery = (query) => {
     self.button_status = 'button_inactive';
     console.log(query);
